b3fc18d61f5b612851172b22af4961599c4fd319
mshaheerz <mshaheerkp9@gmail.com>
2026-05-22 18:11:46 +0530

    build: remove unused tsconfig extends and types

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { voidPlugin } from "void";
 
 export default defineConfig({
-  plugins: [voidPlugin(), react()],
+  plugins: [react()],
   server: {
     host: true,
     port: 5173,
+  },
+  build: {
+    outDir: 'dist',
   },
 })
